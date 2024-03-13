60cee5051c75641d0ac71528227a230f8b530bb9
MarioJimenez <daw15.2026@gmail.com>
2024-03-13 17:11:31 +0100

    proyecto documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,1 +1,36 @@
-package examen;public class Fecha{public static boolean validarFecha(int dia, int mes, int anio){boolean resultado = false;if ((dia>0)&&(mes>0)&&(anio>0)) {if ((mes <=12)&&(dia <=31)) {	if (mes ==2) {resultado = dia <=28;}else if (mes%2 == 0) {resultado = dia <=30;}else {resultado = dia <=31;}}}return resultado;}}
+package examen;
+/**
+ *  La clase fecha gestiona los datos para decir si una fecha es válida o no
+ * 
+ * @author Mario Jiménez
+ * @version 1.0
+ */
+public class Fecha {
+	/**
+	 * @param fechaValida indica la fecha 
+	 */
+	public boolean fechaValida;
+/**
+ * El metodo devuelve con los parametros dados si la fecha es una fecha valida.
+ * 
+ * @param anio indica el año que se le pasa como parametro al método
+ * @param mes indica el mes que se le pasa como parametro al método
+ * @param dia indica el dia que se le pasa como parametro al método
+ * @return variable fechaValida nos dice si la fecha es valida o no
+ */
+	public boolean validarFecha(int anio, int mes, int dia) {
+		fechaValida = false;
+		if ((dia > 0) && (mes > 0) && (anio > 0)) {
+			if ((mes <= 12) && (dia <= 31)) {
+				if (mes == 2) {
+					fechaValida = dia <= 28;
+				} else if (mes % 2 == 0) {
+					fechaValida = dia <= 30;
+				} else {
+					fechaValida = dia <= 31;
+				}
+			}
+		}
+		return fechaValida;
+	}
+}
